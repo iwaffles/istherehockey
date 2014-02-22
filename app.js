c64e1efa465434df95579e973b2f97c54b1d837e
Matt <iwaffles@gmail.com>
2014-02-21 17:14:49 -0800

    Changed listen to accept env variables.

--- app.js
+++ app.js
@@ -19,5 +19,7 @@ app.get('/index.html', function(req, res) {
 	});
 });
 
-app.listen(3000);
-console.log('Listening on port 3000');
+var port = Number(process.env.PORT || 3000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+})
